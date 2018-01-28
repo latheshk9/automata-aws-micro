8af9c307261f49a90434d75620c1dab95dfb90ea
latheshk9 <lathesh.k@kloud9.nyc>
2018-01-28 15:19:22 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+var HelloMessage string
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, HelloMessage)
+}
+
+func main() {
+	Configuration()
+	http.HandleFunc("/", helloWorld)
+	http.ListenAndServe(":8080", nil)
+}
+
+type Config struct {
+	Message string `json:"message"`
+}
+
+func Configuration() {
+	file, err := os.Open("properties.json")
+	if err != nil {
+		fmt.Println("file error:", err)
+		os.Exit(1)
+	}
+	decoder := json.NewDecoder(file)
+	configuration := Config{}
+
+	erro := decoder.Decode(&configuration)
+	if erro != nil {
+		fmt.Println("error:", err)
+		os.Exit(1)
+	}
+	HelloMessage = configuration.Message
+}
