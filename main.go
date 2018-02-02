8b026a8ced066f87c2f7efe490f05589d1518203
latheshk9 <35732885+latheshk9@users.noreply.github.com>
2018-02-02 22:15:26 +0530

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 var HelloMessage string
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
-	HelloMessage := "Hello World New-1"
+	HelloMessage := "Hello World New"
     out, _ := exec.Command("bash", "-c", "hostname").Output()
     HelloMessage = HelloMessage + ": " + string(out)
     fmt.Fprintf(w, HelloMessage)
