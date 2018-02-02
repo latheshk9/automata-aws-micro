dafeddbb75df4cfaa19728d9d3bf5b263fd41473
latheshk9 <35732885+latheshk9@users.noreply.github.com>
2018-02-02 18:37:18 +0530

    Update main.go

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func helloWorld(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	Configuration()
+	//Configuration()
 	http.HandleFunc("/", helloWorld)
 	http.ListenAndServe(":8080", nil)
 }
