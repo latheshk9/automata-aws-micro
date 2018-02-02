92c7bef21902e6abd8e0b5f99b99c0e61517d387
latheshk9 <35732885+latheshk9@users.noreply.github.com>
2018-02-02 20:11:29 +0530

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 var HelloMessage string
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
-	HelloMessage := "Hello World BDD"
+	HelloMessage := "Hello World BDD Fail"
     out, _ := exec.Command("bash", "-c", "hostname").Output()
     HelloMessage = HelloMessage + ": " + string(out)
     fmt.Fprintf(w, HelloMessage)
