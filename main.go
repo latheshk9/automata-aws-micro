c558840ac0b1a9dd28af3e4b71f05d36c9f06264
latheshk9 <35732885+latheshk9@users.noreply.github.com>
2018-02-07 03:08:45 +0530

    Update main.go

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
 }
 
 func getHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "HELLO WORLD!")
+	fmt.Fprintf(w, "HELLO WORLD NEW!")
 }
 
 // fail writes a json response with error msg and status header
